7ae1b9534cf52fa5dd055393f7cd415eb3416728
CodePriyansh <saipriyanshu786@gmail.com>
2022-05-11 23:52:35 +0530

    auth

--- routes/admin/orderHistory.route.js
+++ routes/admin/orderHistory.route.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const auth = require("../../Authorization/userAuth.token")
 const orderController = require('../../controller/admin-controller/order.ctrl')
-router.get("/viewOrder",tokenVerification.varifyToken, orderController.orderDetail)
+router.get("/viewOrder",auth.verifyToken, orderController.orderDetail)
 module.exports = router;
