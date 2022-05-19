24a0a4e8d11434d9cd5ee20dc942e9254567b3a3
mohitibfoundation <mohit.ibfoundation@gmail.com>
2022-05-19 10:36:43 +0530

    dfg

--- controller/user-controller/cart.ctrl.js
+++ controller/user-controller/cart.ctrl.js
@@ -2,11 +2,11 @@ const Cart = require("../../models/customer-model/cart.model");
 exports.AddToCart = async (request, response) => {
   console.log(request.body);
 
-  var cart = await Cart.findOne({ customerId: request.body.cusId });
+  var cart = await Cart.findOne({ customerId: request.body.userId });
 
   if (!cart) {
     cart = new Cart({
-      customerId: request.body.cusId,
+      customerId: request.body.userId,
     });
   }
 
@@ -25,15 +25,16 @@ exports.AddToCart = async (request, response) => {
 
 exports.DeleteCartItem = (request, response) => {
   Cart.updateOne(
-    { customerId: request.body.cusId },
+    { customerId: request.body.userId },
     {
       $pullAll: {
-        cartItems: [request.body.pId],
+        cartItems: [request.body.id],
       },
     }
   )
     .then((result) => {
-      return response.status(200).json(result);
+      return response.status(200).json({status:'ok',
+      current_user:result});
     })
     .catch((err) => {
       return response.status(500).json(err);
@@ -41,10 +42,11 @@ exports.DeleteCartItem = (request, response) => {
 };
 
 exports.ViewCart = (request, response) => {
-  Cart.findOne({ customerId: request.body.cusId })
+  Cart.findOne({ customerId: request.body.userId })
     .populate("cartItems")
     .then((result) => {
-      return response.status(200).json(result);
+      return response.status(200).json({status:'ok',
+      current_user:result});
     })
     .catch((err) => {
       return response.status(500).json(err);
@@ -52,9 +54,10 @@ exports.ViewCart = (request, response) => {
 };
 
 exports.DeleteCart = (request, response) => {
-  Cart.deleteOne({ customerId: request.body.cusId })
+  Cart.deleteOne({ customerId: request.body.userId })
     .then((result) => {
-      return response.status(200).json(result);
+      return response.status(200).json({status:'ok',
+    current_user:result});
     })
     .catch((err) => {
       return response.status(500).json(err);
